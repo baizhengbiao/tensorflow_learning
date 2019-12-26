01c2b967e9b5da3301a3eaee47b2ddeb36b763ce
baizhengbiao <baizhengbiao@163.com>
2019-12-26 19:24:52 +0800

    add test file

---
+++ test_version.py
@@ -0,0 +1,6 @@
+# encoding: utf-8
+import tensorflow as tf
+a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+c = tf.matmul(a, b)
+print (c)
